534f1fbabc6a1b4629b60173285ee4ab05cd5c19
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-02-12 21:19:53 -0500

    Add more dotfiles

---
+++ dot_config/nixpkgs/flake.nix
@@ -0,0 +1,56 @@
+{
+  description = "Home Manager Flake";
+
+  inputs = {
+    nixpkgs.url = "flake:nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = {nixpkgs, home-manager, ...}:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; }; 
+    in {
+    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
+
+    homeConfigurations =
+      let
+        ocamlPackages = pkgs.recurseIntoAttrs pkgs.ocaml-ng.ocamlPackages_latest;
+      in {
+      "ysthakur" = home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.x86_64-linux;
+
+        modules = [
+          {
+            home.username = "ysthakur";
+            home.homeDirectory = "/home/ysthakur";
+            programs.home-manager.enable = true;
+
+            home.packages = [
+              pkgs.chezmoi
+              # Like Powershell but for Linux
+              pkgs.nushell
+              pkgs.ruby_3_1
+              pkgs.graalvm17-ce
+              # Build system for OCaml
+              #pkgs.dune_2
+            ] ++ (with ocamlPackages; [
+              #ocaml
+              #findlib
+              #core
+              #ounit
+              #utop
+              #qcheck
+            ]);
+
+            home.stateVersion = "22.11";
+          }
+        ];
+      };
+    };
+  };
+}
+
