6ec236d800a65869a1d523eb37a3dfc75e66d218
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-02-16 00:30:59 -0500

    Make home-manager work with multiple devices

--- dot_config/nixpkgs/flake.nix
+++ dot_config/nixpkgs/flake.nix
@@ -12,49 +12,53 @@
   outputs = {nixpkgs, home-manager, ...}:
     let
       system = "x86_64-linux";
-      pkgs = import nixpkgs { inherit system; }; 
-    in {
-    defaultPackage.x86_64-linux = home-manager.defaultPackage.x86_64-linux;
-
-    homeConfigurations =
-      let
-        ocamlPackages = pkgs.recurseIntoAttrs pkgs.ocaml-ng.ocamlPackages_latest;
-      in {
-      "ysthakur" = home-manager.lib.homeManagerConfiguration {
-        pkgs = nixpkgs.legacyPackages.x86_64-linux;
-
+      pkgs = import nixpkgs { inherit system; };
+      ocamlPackages = pkgs.recurseIntoAttrs pkgs.ocaml-ng.ocamlPackages_latest;
+      # Helper with common config
+      baseConfig = {username, extraPkgs ? []}: {
+        pkgs = nixpkgs.legacyPackages.${system};
         modules = [
           {
-            home.username = "ysthakur";
-            home.homeDirectory = "/home/ysthakur";
             programs.home-manager.enable = true;
-
-            home.packages = [
-              # For managing dotfiles
-              pkgs.chezmoi
-              # GitHub CLI
-              pkgs.gh
-              # Prettier ls alternative
-              pkgs.exa
-              # Show disk usage visually
-              pkgs.ncdu
-              pkgs.ruby_3_1
-              # Build system for OCaml
-              #pkgs.dune_2
-            ] ++ (with ocamlPackages; [
-              #ocaml
-              #findlib
-              #core
-              #ounit
-              #utop
-              #qcheck
-            ]);
-
-            home.stateVersion = "22.11";
+            home = {
+              username = username;
+              homeDirectory = "/home/${username}";
+              packages = [
+                # For managing dotfiles
+                pkgs.chezmoi
+                # GitHub CLI
+                pkgs.gh
+                # Prettier ls alternative
+                pkgs.exa
+                # Show disk usage visually
+                pkgs.ncdu
+              ] ++ extraPkgs;
+              stateVersion = "22.11";
+            };
           }
         ];
       };
+    in {
+      defaultPackage.${system} = home-manager.defaultPackage.${system};
+
+      homeConfigurations.yash = home-manager.lib.homeManagerConfiguration (baseConfig {
+        username = "yash";
+      });
+      homeConfigurations.ysthakur = home-manager.lib.homeManagerConfiguration (baseConfig {
+        username = "ysthakur";
+        extraPkgs = [
+            pkgs.ruby_3_1
+            # Build system for OCaml
+            #pkgs.dune_2
+          ] ++ (with ocamlPackages; [
+            #ocaml
+            #findlib
+            #core
+            #ounit
+            #utop
+            #qcheck
+          ]);
+      });
     };
-  };
 }
 
