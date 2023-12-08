00684898e21b3ebb36bf281437fd432c63387a53
ysthakur <45539777+ysthakur@users.noreply.github.com>
2023-12-08 17:17:51 -0500

    Make common nix file for wsl configs

--- dot_config/home-manager/silver-hp2.nix
+++ dot_config/home-manager/silver-hp2.nix
@@ -1,36 +1,8 @@
 { pkgs, util }:
 
-let theiaPackages = with pkgs; [ sqlite sqlite.dev ];
-in
-{
-  ysthakur = util.createConfig {
-    username = "ysthakur";
-    hostname = "silver-hp2";
-    extraPkgs = (with pkgs; [
-        # For image metadata
-        exif
-        # Go language
-        go
-        # GitHub CLI
-        gh
-        # For recording terminal sessions
-        asciinema
-        # For CMSC 430
-        nasm
-        (texlive.combine {
-          inherit (pkgs.texlive) scheme-basic
-          latexmk latexindent titlesec datetime parskip etoolbox mathtools fmtcount xkeyval soul listings;
-        })
-        # Alternative to LaTeX
-        typst
-        # File explorer
-        xfce.thunar
-      ]) ++ theiaPackages;
-    extra = {
-      home.sessionVariables = {
-        GOBIN = "/home/ysthakur/go/bin";
-      };
-    };
-  };
-}
+(import ./wsl.nix {
+  inherit pkgs util;
+  username = "ysthakur";
+  hostname = "silver-hp2";
+})
 
